d874ee5df204ccb3441ad7b39c35a4a68a36d811
Emil Abusev <emilabusev.yandex.ru>
2024-01-24 19:56:07 +0300

    Дополняет swiper-hero, добавляет стили для (nav, hero)

--- source/js/modules/swiper-slider/hero-swiper-slider.js
+++ source/js/modules/swiper-slider/hero-swiper-slider.js
@@ -1,25 +1,39 @@
 import Swiper from 'swiper/bundle';
+import { Pagination } from 'swiper/modules';
 
 
 const sliderHero = document.querySelector('[data-hero-slider="hero-slider"]');
-const swiperHeroPagination = document.querySelector('[data-hero-pagination="hero-swiper-pagination"]');
 
-const swiperHero = () => new Swiper(sliderHero, {
-  pagination: {
-    el: swiperHeroPagination,
-    clickable: true,
-    type: 'bullets',
-    bulletElement: 'div',
-    bulletClass: 'hero__pagination-bullet',
-    bulletActiveClass: 'hero__pagination-bullet--active',
-  },
-  loop: true,
-  breakpoints: {
-    1440: {
-      allowTouchMove: false,
+const swiperHero = () => {
+  const slider = new Swiper(sliderHero, {
+    observer: true,
+    modules: [Pagination],
+    pagination: {
+      el: '.swiper-slide-active .hero__pagination',
+      clickable: true,
+      type: 'bullets',
+      bulletElement: 'div',
+      bulletClass: 'hero__pagination-bullet',
+      bulletActiveClass: 'hero__pagination-bullet--active',
     },
-  },
-});
+    loop: true,
+    breakpoints: {
+      1440: {
+        allowTouchMove: false,
+      },
+    },
+    // autoplay: {
+    //   delay: 3000,
+    // },
+    on: {
+      slideChangeTransitionStart: () => {
+        slider.pagination.init();
+        slider.pagination.render();
+        slider.pagination.update();
+      }
+    }
+  });
+};
 
 const initSwiperHero = () => {
   if (document.body.contains(sliderHero)) {
